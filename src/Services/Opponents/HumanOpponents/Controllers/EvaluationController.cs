5b7bd4170e43481fa321962e5b9b35f06735bfb1
Rajib <forrajib@gmail.com>
2021-05-06 01:43:45 -0400

    create Opponents

---
+++ src/Services/Opponents/HumanOpponents/Controllers/EvaluationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HumanOpponents.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EvaluationController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Evaluate")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<string> Evaluate([FromBody] Dictionary<string, string> games)
+        {
+            //do logic here then return the winnerId 
+            return Ok("winnerId");
+        }
+
+    }
+}
